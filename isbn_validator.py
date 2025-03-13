41f9390d3bfcaacea436b80648d9f4fa7325c85d
DerRheingauMax <max.rheingauschule@gmail.com>
2025-03-13 13:03:38 +0100

    Update isbn_validator.py

--- isbn_validator.py
+++ isbn_validator.py
@@ -1,29 +1,33 @@
 test = "978-3-8668-0192-9"
+
+
 def validate_isbn(isbn):
     if not valid_isbn_format(isbn):
         return False
     isbn_number = ""
-    prove_number =0 
+    prove_number = 0
     for i in isbn:
-        if i != "-": isbn_number += i
+        if i != "-":
+            isbn_number += i
     counter = 0
     for index, i in enumerate(isbn_number):
         if index + 1 == 13:
             break
-        if (index + 1) %2 == 0:
-            prove_number += (int(i) *3)
+        if (index + 1) % 2 == 0:
+            prove_number += int(i) * 3
         else:
             prove_number += int(i)
-        counter += 1      
+        counter += 1
     if 10 - prove_number % 10 == int(isbn_number[-1]):
         return True
     else:
         return False
-    
+
 
 def valid_isbn_format(isbn):
-    dash_indexes = [3,5,10,15]
-    if len(isbn) != 17 : return False
+    dash_indexes = [3, 5, 8, 15]
+    if len(isbn) != 17:
+        return False
     try:
         for index, i in enumerate(isbn):
 
@@ -35,3 +39,11 @@ def valid_isbn_format(isbn):
         return False
     return True
 
+
+if __name__ == "__main__":
+    while True:
+        isbn = input("isbn: ")
+        if isbn == "":
+            break
+        if not valid_isbn_format(isbn):
+            continue
